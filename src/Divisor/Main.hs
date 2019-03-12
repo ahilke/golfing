77b093c04929b13aa9ed69ebba304a5d8fdf7207
Arno Hilke <arno.hilke@tum.de>
2019-03-12 05:32:45 +0100

    Cleanup of Haskell Divisor solution.

--- src/Divisor/Main.hs
+++ src/Divisor/Main.hs
@@ -2,39 +2,20 @@ module Divisor.Main where
 
 import Data.List
 
--- list of multiples of x less or equal to 100
-get_multiples_to_100 :: Int -> [Int]
-get_multiples_to_100 x = [x, 2 * x .. 100]
+-- Returns list of divisors of n.
+get_divisors :: Int -> [Int]
+get_divisors n = [x | x <- [1..n], mod n x == 0]
 
--- list of multiples for numbers 1 - 100
-multiples_to_100 :: [[Int]]
-multiples_to_100 = [get_multiples_to_100 x | x <- [1..100]]
+-- List of divisors for 1 to 100.
+divisors :: [[Int]]
+divisors = [get_divisors x | x <- [1..100]]
 
-empty_to_100 :: [[Int]]
-empty_to_100 = [[] | x <- [1..100]]
-
--- invert multiples array?
-
-divisors_to_100 :: [[Int]]
-divisors_to_100 = [get_multiples_to_100 x | x <- [1..100]]
-
-divisors_to_100_string :: [[String]]
-divisors_to_100_string = [[show number | number <- divisor_list] | divisor_list <- divisors_to_100]
-
+-- List of divisors as space-separated strings.
 divisor_lines :: [String]
-divisor_lines = [(intercalate " " list) | list <- divisors_to_100_string]
+divisor_lines = [intercalate " " [show number | number <- divisor_list] | divisor_list <- divisors]
 
 divisor_output :: String
-divisor_output = intercalate "\n" divisor_lines ++ "\n"
-
-get_divisors :: Int -> [Int]
-get_divisors n = [x | x <- [1..100], mod n x == 0]
-
-real_divisors :: [[Int]]
-real_divisors = [get_divisors x | x <- [1..100]]
-
-real_divisor_output :: String
-real_divisor_output = intercalate "\n" [intercalate " " [show number | number <- divisor_list] | divisor_list <- real_divisors]
+divisor_output = intercalate "\n" divisor_lines
 
 main :: IO()
-main = putStrLn real_divisor_output
+main = putStrLn divisor_output
