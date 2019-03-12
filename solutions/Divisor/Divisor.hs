eb55eba1290c55411db03be841afe97b0c53f76c
Arno Hilke <arno.hilke@tum.de>
2019-03-12 05:48:03 +0100

    Haskell Divisor: Alias intercalate (118 -> 112).

--- solutions/Divisor/Divisor.hs
+++ solutions/Divisor/Divisor.hs
@@ -1,2 +1,3 @@
 import Data.List
-main=putStrLn(intercalate"\n"[intercalate" "[show n|n<-z]|z<-[[x|x<-[1..y],mod y x==0]|y<-[1..100]]])
+a=intercalate
+main=putStrLn(a"\n"[a" "[show n|n<-z]|z<-[[x|x<-[1..y],mod y x==0]|y<-[1..100]]])
