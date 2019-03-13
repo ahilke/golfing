1b3d0cedb37be2dc238fcd05fbae8c75f7d88b79
Arno Hilke <arno.hilke@tum.de>
2019-03-13 02:01:35 +0100

    PHP Divisor: Use actual linebreak instead of \n (63 -> 62).

--- solutions/Divisor/Divisor.php
+++ solutions/Divisor/Divisor.php
@@ -1,2 +1,3 @@
 <?php // this line is not counted, it just enables syntax highlighting
-while($i++<100){$c=0;while($c++<$i)$i%$c||print"$c ";echo"\n";}
+while($i++<100){$c=0;while($c++<$i)$i%$c||print"$c ";echo"
+";}
