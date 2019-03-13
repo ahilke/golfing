b5eb7a25ea1ef453b9766e7dc86453ad968fc08a
Arno Hilke <arno.hilke@tum.de>
2019-03-13 01:42:32 +0100

    PHP Divisor: Use template string instead of concatenation (71 -> 70).

--- solutions/Divisor/Divisor.php
+++ solutions/Divisor/Divisor.php
@@ -1,2 +1,2 @@
 <?php // this line is not counted, it just enables syntax highlighting
-while($i<100){$i++;$c=0;while($c<$i)$c++&&$i%$c||print$c.' ';echo"\n";}
+while($i<100){$i++;$c=0;while($c<$i)$c++&&$i%$c||print"$c ";echo"\n";}
