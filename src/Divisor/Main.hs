c9354433c7cfe783a4663d70db87db53ddcd1ebb
Arno Hilke <arno.hilke@tum.de>
2019-03-13 02:13:57 +0100

    Haskell Divisor: Adjust long version to new solution.

--- src/Divisor/Main.hs
+++ src/Divisor/Main.hs
@@ -3,19 +3,19 @@ module Divisor.Main where
 import Data.List
 
 -- Returns list of divisors of n.
-get_divisors :: Int -> [Int]
-get_divisors n = [x | x <- [1..n], mod n x == 0]
+get_divisors :: Int -> [String]
+get_divisors n = [show x | x <- [1..n], mod n x == 0]
 
 -- List of divisors for 1 to 100.
-divisors :: [[Int]]
+divisors :: [[String]]
 divisors = [get_divisors x | x <- [1..100]]
 
 -- List of divisors as space-separated strings.
 divisor_lines :: [String]
-divisor_lines = [intercalate " " [show number | number <- divisor_list] | divisor_list <- divisors]
+divisor_lines = [intercalate " " divisor_list | divisor_list <- divisors]
 
 divisor_output :: String
 divisor_output = intercalate "\n" divisor_lines
 
 main :: IO()
-main = putStrLn divisor_output
+main = putStr divisor_output
