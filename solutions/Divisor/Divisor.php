f8b63c3e083a85b91952aa6737817bdaee5d551a
Arno Hilke <arno.hilke@tum.de>
2019-03-14 00:18:29 +0100

    PHP Divisor: Remove unnecessary semicolon (61 -> 60).

--- solutions/Divisor/Divisor.php
+++ solutions/Divisor/Divisor.php
@@ -1,3 +1,4 @@
-<?php // this line is not counted, it just enables syntax highlighting
+<?php // opening tag is not required by code-golf.io, it just enables syntax highlighting
 $i=1;while($c++<100||$i++<100&&$c=print"
-")$i%$c||print"$c ";
+")$i%$c||print"$c "
+; // closing semicolon is also not required by code-golf.io
