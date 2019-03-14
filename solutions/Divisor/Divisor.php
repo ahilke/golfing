56d592b68505a4de37aa71427b19f3fc7e85c505
Arno Hilke <arno.hilke@tum.de>
2019-03-14 01:06:34 +0100

    PHP Divisor: Replace && with + (59 -> 57).
    This has two effects:
    1. It chains the two statements with one instead of two characters.
    2. Since the value of $c/print is 1, it decreases 100 to 99, saving another character.

--- solutions/Divisor/Divisor.php
+++ solutions/Divisor/Divisor.php
@@ -1,4 +1,4 @@
 <?php // opening tag is not required by code-golf.io, it just enables syntax highlighting
-$i=1;while($c++<$i||$i++<100&&$c=print"
+$i=1;while($c++<$i||$i++<99+$c=print"
 ")$i%$c||print"$c "
 ; // closing semicolon is also not required by code-golf.io
