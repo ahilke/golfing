2454ad2c60154407b1fb4297f86a356265438baa
Arno Hilke <arno.hilke@tum.de>
2019-03-14 02:58:04 +0100

    PHP Evil Numbers: Use for instead of while (61 -> 60).
    Adjust long solution to use count_chars.

--- solutions/Evil Numbers/evil_numbers.php
+++ solutions/Evil Numbers/evil_numbers.php
@@ -1,4 +1,4 @@
 <?php
-$i=-1;while($i++<50)count_chars(decbin($i))[49]%2||print"$i
-"
+for($i=-1;$i++<50;count_chars(decbin($i))[49]%2||print"$i
+")
 ;
--- src/Evil Numbers/evil_numbers.php
+++ src/Evil Numbers/evil_numbers.php
@@ -3,8 +3,8 @@
 $i = -1;
 while ($i++ < 50) {
     $hex = decbin($i);
-    $hexArray = count_chars($hex);
-    if ($hexArray[49] % 2 === 0) {
+    $chars = count_chars($hex);
+    if ($chars[49] % 2 === 0) {
         echo "$i\n";
     }
 }
