25241463f8ec84006a31f2785f3fe8a7d231f20e
Arno Hilke <arno.hilke@tum.de>
2019-03-14 01:32:12 +0100

    PHP Divisor: Use for instead of while (57 -> 56).
    Two characters saved by using for instead of while, but requiring one additional semicolon.

--- solutions/Divisor/Divisor.php
+++ solutions/Divisor/Divisor.php
@@ -1,4 +1,4 @@
 <?php // opening tag is not required by code-golf.io, it just enables syntax highlighting
-$i=1;while($c++<$i||$i++<99+$c=print"
-")$i%$c||print"$c "
+for($i=1;$c++<$i||$i++<99+$c=print"
+";$i%$c||print"$c ")
 ; // closing semicolon is also not required by code-golf.io
