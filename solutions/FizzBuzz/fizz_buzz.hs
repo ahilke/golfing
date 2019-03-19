60fb3ebc809d4b125e13757cbba875526fbfbee8
Arno Hilke <arno.hilke@tum.de>
2019-03-19 23:39:54 +0100

    Haskell FizzBuzz: Alias functions (145 -> 126).

--- solutions/FizzBuzz/fizz_buzz.hs
+++ solutions/FizzBuzz/fizz_buzz.hs
@@ -1,2 +1,4 @@
-f n|mod n 3==0&&mod n 5==0=putStrLn"FizzBuzz"|mod n 3==0=putStrLn"Fizz"|mod n 5==0=putStrLn"Buzz"|otherwise=putStrLn(show n)
+p=putStrLn
+m=mod
+f n|m n 3==0&&m n 5==0=p"FizzBuzz"|m n 3==0=p"Fizz"|m n 5==0=p"Buzz"|otherwise=p(show n)
 main=mapM_ f[1..100]
