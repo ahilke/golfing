12ffa16125d494ab48a9e778aeac35c5527ff464
Arno Hilke <arno.hilke@tum.de>
2019-03-19 23:51:27 +0100

    Haskell FizzBuzz: Use $ instead of () (117 -> 116).
    Define execution order with $ instead of parentheses.

--- solutions/FizzBuzz/fizz_buzz.hs
+++ solutions/FizzBuzz/fizz_buzz.hs
@@ -1,4 +1,4 @@
 p=putStrLn
 m=mod
-f n|m n 15==0=p"FizzBuzz"|m n 3==0=p"Fizz"|m n 5==0=p"Buzz"|otherwise=p(show n)
+f n|m n 15==0=p"FizzBuzz"|m n 3==0=p"Fizz"|m n 5==0=p"Buzz"|otherwise=p$show n
 main=mapM_ f[1..100]
