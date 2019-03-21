0d0da9b87753f627027751ae60eaa6b90f727ed3
Arno Hilke <arno.hilke@tum.de>
2019-03-21 02:21:43 +0100

    PHP Pascal's Triangle: Combine both for loops into one (144 -> 143).

--- solutions/Pascal's Triangle/pascals_triangle.php
+++ solutions/Pascal's Triangle/pascals_triangle.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('error_reporting', E_ERROR | E_PARSE);
 
-function p($r,$c){if($c==1)return 1;if($c==$r)return 1;return p(--$r,$c)+p($r,--$c);}for(;$r++<20;print"
-")for($c=0;$c++<$r;)echo p($r, $c),' ';
+function p($r,$c){if($c==1)return 1;if($c==$r)return 1;return p(--$r,$c)+p($r,--$c);}for($r=1;$c++<$r||$r++<19+$c=print"
+";)echo p($r, $c),' ';
 
 ;
