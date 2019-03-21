0b8cd0fe3ece6a9ed6b0c127d47c47f03439eab9
Arno Hilke <arno.hilke@tum.de>
2019-03-21 02:32:39 +0100

    PHP Pascal's Triangle: Use ternary shorthand (121 -> 120).
    Return true instead of 1 with ternary shorthand, which will be cast to 1.

--- solutions/Pascal's Triangle/pascals_triangle.php
+++ solutions/Pascal's Triangle/pascals_triangle.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('error_reporting', E_ERROR | E_PARSE);
 
-function p($r,$c){return $c==1||$c==$r?1:p(--$r,$c)+p($r,--$c);}for($r=1;$c++<$r||$r++<19+$c=print"
+function p($r,$c){return $c==1||$c==$r?:p(--$r,$c)+p($r,--$c);}for($r=1;$c++<$r||$r++<19+$c=print"
 ";)echo p($r,$c),' ';
 
 ;
